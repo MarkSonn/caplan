882d4c386a73081f630a8a3e80f2480fef503b6d
nugycal <nugycal@gmail.com>
2019-07-27 18:29:08 +1000

    Map kinda working but not really

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,6 +6,10 @@
 
 <script>
 var H = window.H
+let lng = 0
+let lat = 0
+let obj = 0
+let inst = 0
 
 export default {
   name: 'Map',
@@ -23,35 +27,42 @@ export default {
     // Obtain the default map types from the platform object
     var layers = this.platform.createDefaultLayers()
 
-    this.lat = 0
-    this.lng = 0
+    obj = this.$refs.map
 
     if (navigator.geolocation) {
-      let lft = 0
-      let lmg = 0
       navigator.geolocation.getCurrentPosition(function (callback) {
-        lft = callback.coords.latitude
-        lmg = callback.coords.longitude
+        lat = callback.coords.latitude
+        lng = callback.coords.longitude
+        console.log(lat, lng)
+        obj.innerHTML = ''
+        inst = new H.Map(
+          obj,
+          layers.vector.normal.map,
+          {
+            zoom: Math.floor(callback.coords.accuracy / 5),
+            center: { lng: lng, lat: lat }
+          })
       })
-      this.lat = lft
-      this.lng = lmg
     } else {
       alert('Unable to obtain user location (Geolocation not supported)')
     }
 
     // Instantiate (and display) a map object:
-    this.inst = new H.Map(
-      this.$refs.map,
-      layers.vector.normal.map,
-      {
-        zoom: 10,
-        center: { lng: this.lng, lat: this.lat }
-      })
+    if (lat == 0) {
+      inst = new H.Map(
+        this.$refs.map,
+        layers.vector.normal.map,
+        {
+          zoom: 10,
+          center: { lng: lng, lat: lat }
+        })
+    }
 
-    /*window.addEventListener('resize', () => this.$refs.map.getViewPort().resize())
-    /*var mapEvents = new H.mapevents.MapEvents(this.inst)
+    // window.addEventListener('resize', () => this.$refs.map.getViewPort().resize())
+    var mapEvents = new H.mapevents.MapEvents(this.inst)
     var behavior = new H.mapevents.Behavior(mapEvents)
-    var ui = H.ui.UI.createDefault(this.inst, layers)*/
+    var ui = H.ui.UI.createDefault(inst, layers)
   }
 }
+
 </script>
