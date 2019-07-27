4ff555aa6206379c234fec59af6ab7581a56917e
Bryan W Hutagalung <46338456+bryanwh@users.noreply.github.com>
2019-07-27 22:40:57 +1000

    button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,36 +1,52 @@
 <template>
-  <v-app-bar
-    color="#c22300" dark app>
-    <v-toolbar-title>Yeats</v-toolbar-title>
-
-    <v-spacer />
-
-    <!-- <v-btn icon><v-icon>mdi-heart</v-icon></v-btn>
-    <v-btn icon><v-icon>mdi-magnify</v-icon></v-btn>
-
-    <v-menu
-      left
-      bottom>
-      <template v-slot:activator="{ on }">
-        <v-btn v-on="on" icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list>
-        <v-list-item
-          v-for="n in 5"
-          @click="() => {}"
-          :key="n">
-          <v-list-item-title>Option {{ n }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu> -->
-  </v-app-bar>
+  <div>
+    <v-app-bar
+      color="#c22300" dark app>
+      <v-toolbar-title>Yeats<span v-if='user'> | <small>{{ user }}</small></span></v-toolbar-title>
+      <v-spacer />
+      <v-btn v-if="loggedIn" @click="logOut()" color="#800000">
+        Log Out<v-icon right>mdi-lock</v-icon>
+      </v-btn>
+      <v-btn v-else @click="googleSignIn()" color="#800000">
+        Log In<v-icon right>mdi-lock-open</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  data: () => ({
+    themeColor: '#c22300',
+    themeColorDark: '#b30000',
+    loggedIn: false,
+    user: ''
+  }),
+  methods: {
+    async googleSignIn() {
+      console.log('logging in')
+      var provider = new firebaseCore.auth.GoogleAuthProvider()
+      try {
+        const result = await auth.signInWithPopup(provider)
+        // var token = result.credential.accessToken
+        this.user = result.user.displayName
+        this.loggedIn = true
+        console.log('lol', this.user)
+      } catch (error) {
+        console.log('Auth oopsies', error)
+      }
+    },
+    async logOut () {
+      console.log('logging out')
+      try {
+        await auth.signOut()
+        this.loggedIn = false
+        this.user = ''
+      } catch (error) {
+        console.log('Oh no, could not log out', error)
+      }
+    }
+  }
 }
 </script>
