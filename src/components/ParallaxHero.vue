34b158dbf660d8a1988d64c8bef9c87c1625f22e
Mark Sonnenschein <marksonn5@gmail.com>
2019-07-27 16:46:58 +1000

    Pushed matt's code

--- src/components/ParallaxHero.vue
+++ src/components/ParallaxHero.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    TODO: ParallaxHero
+    <v-parallax
+        dark
+        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-layout
+          align-center
+          column
+          justify-center>
+          <h1 class="display-2 font-weight-thin mb-4">Yeats</h1>
+          <h4 class="subheading">First you eat it, then you yeat it!</h4>
+        </v-layout>
+      </v-parallax>
   </div>
 </template>
 
